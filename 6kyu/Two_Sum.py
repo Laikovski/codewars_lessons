413d7d53db8d71e50b152de0fa420d727f05bed4
laikov <zheka-se@ya.ru>
2021-05-04 00:10:46 +0300

    pin

--- 6kyu/Two_Sum.py
+++ 6kyu/Two_Sum.py
@@ -11,6 +11,6 @@
 def two_sum(numbers, target):
     d = {}
     for i ,n in enumerate(numbers):
-        if target -n in d:
+        if target - n in d:
             return [d[target -n] ,i]
         d[n] = i
